7472d79110cfe5ad1219c723a0cd30fcb7565b88
陈泽华 <136565629@qq.com>
2023-05-23 16:06:07 +0800

    提交接口编写

--- lesson6/shop/src/main/java/ltd/newbee/mall/controller/MyBatisController.java
+++ lesson6/shop/src/main/java/ltd/newbee/mall/controller/MyBatisController.java
@@ -37,6 +37,24 @@ public class MyBatisController {
 //    修改一条记录
 
     @GetMapping("/users/mybatis/update")
-    public  Boolean
+    public  Boolean insert(Integer id,String name,String password){
+        if(id==null||id<1||StringUtils.isEmpty(name)||StringUtils.isEmpty(password)){
+            return false;
+        }
+        User user=new User();
+        user.setId(id);
+        user.setName(name);
+        user.setPassword(password);
+        return userDao.updUser(user)>0;
+    }
+
+    // 删除一条记录
+    @GetMapping("/user/mybatis/delete")
+    public  Boolean insert(Integer id){
+        if(id==null||id<1){
+            return false;
+        }
+        return userDao.delUser(id)>0;
+    }
 
 }
